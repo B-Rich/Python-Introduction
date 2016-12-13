a754f18e749c65ab8bbf75ccc0acc2e177b04d9a
Mehmet Yıldız <xmyildiz@gmail.com>
2016-12-13 16:27:59 +0300

    Arithmetics
    - Added “Arithmetics” Program.

---
+++ Arithmetics.py
@@ -0,0 +1,38 @@
+# Arithmetic Operations
+#   +   plus            addition
+#   -   minus           subtraction
+#   *   times           multiplication
+#   /   divided by      subtraction
+#   **   power          exponentiation
+
+# Operator Precedence = (), **, *, /, +, -
+
+print (3+4) # Prints 7
+print (type(3+4)) # int
+
+print (3-2) # Prints 1
+print (type(3-2)) # int
+
+print (3*4) # Prints 12
+print (type(3*4)) # Prints 12
+
+print (9/3) # Prints 3.0
+print (type(9/3)) # float
+
+print (9/4) # Prints 2.25
+print (type(9/4)) # float
+
+print (9.0/3.0) # Prints 3.0
+print (type(9.0/3.0)) # float
+
+print (3**4) # Prints 81
+print (type(3**4)) # int
+
+# Variable Incrementation
+
+x = 1
+print (x) # 1
+x = x + 1
+print (x) # 2
+x += 1;
+print (x) # 3
